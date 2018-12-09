e88ca0ffe53174b0a35e0dda4a2b5e4b3b1b4e8c
Romain Prévost <32127074+toindev@users.noreply.github.com>
2018-12-09 22:23:24 +0100

    Script de lancement

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# À modifier avec l'IP ou le nom d'hôte de la machine
+export PROXY_HOST=enk00.northeurope.cloudapp.azure.com
+
+export PROXY_PORT=80
+
+docker-compose -p enk up -d
+
+#Permet de faire du nettoyage des potentiels précédents lancements, sans
+#supprimer les images nécessaires (vu qu'elles sont déjà lancées)
+docker system prune --all --volumes
