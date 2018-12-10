1343efa9db6f699652780b0c0691bd425a80e686
PREVOST Romain <Romain.PREVOST@sword-group.com>
2018-12-10 13:46:31 +0100

    Dos2Unix et dossier non-relatif

--- start_remote_vm.sh
+++ start_remote_vm.sh
@@ -2,5 +2,8 @@
 
 HOST=$1.northeurope.cloudapp.azure.com
 
-ssh enk@HOST sudo mv /formation-ENK /home/enk/
-ssh enk@HOST sudo bash ~/formation-ENK/start.sh HOST
+echo ssh enk@$HOST sudo mv /formation-ENK /home/enk/
+ssh enk@$HOST sudo mv /formation-ENK /home/enk/
+
+echo ssh enk@$HOST sudo bash ~/formation-ENK/start.sh $HOST
+ssh enk@$HOST sudo bash /home/enk/formation-ENK/start.sh $HOST
