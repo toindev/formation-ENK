0efbe78871296cac36281ef62da943b73cf04a0f
Romain Prévost <32127074+toindev@users.noreply.github.com>
2018-12-10 12:57:31 +0100

    Hôte variabilisé et workdir défini

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# À modifier avec l'IP ou le nom d'hôte de la machine
-export PROXY_HOST=enk00.northeurope.cloudapp.azure.com
+# C'est le dossier référence d'installation
+cd /formation-ENK
+
+# Le premier argument du script est le nom d'hôte
+export PROXY_HOST=$1
 
 export PROXY_PORT=80
 
@@ -9,6 +12,6 @@ docker-compose -p enk stop
 docker-compose -p enk rm
 docker-compose -p enk up -d
 
-#Permet de faire du nettoyage des potentiels précédents lancements, sans
-#supprimer les images nécessaires (vu qu'elles sont déjà lancées)
+# Permet de faire du nettoyage des potentiels précédents lancements, sans
+# supprimer les images nécessaires (vu qu'elles sont déjà lancées)
 docker system prune --all --volumes
