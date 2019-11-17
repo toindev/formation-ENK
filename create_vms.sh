e90bbd30ab0124cbc66f09c6f086bd6c0b94d886
Romain Prévost <32127074+toindev@users.noreply.github.com>
2019-11-17 17:38:25 +0100

    Pick ssh key from the current account

--- create_vms.sh
+++ create_vms.sh
@@ -3,9 +3,10 @@ INSTANCE=$1
 az vm create \
 --size Standard_D2_v3 \
 --resource-group insa_2018 \
---image ENK \
+--image ENK_IMAGE \
 --admin-username enk \
 --nsg enk_nsg \
---ssh-key-value id_rsa.pub \
+--ssh-key-value /home/prevost/.ssh/id_rsa.pub \
 --name $INSTANCE \
 --public-ip-address-dns-name $INSTANCE
+
